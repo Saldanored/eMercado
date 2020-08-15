e8a99b0b982321da77cae4a9c1d4989ba623a3e8
Saldanored <aldo_515@hotmail.com>
2020-08-15 20:33:55 -0300

    Google login added

--- js/login.js
+++ js/login.js
@@ -4,6 +4,17 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  sessionStorage.setItem('key', 'accessoPermitido'); 
+  window.location.href="index.html";
+}
+
 function fakeLogin(){
     sessionStorage.setItem('key', 'accessoPermitido'); 
   }
